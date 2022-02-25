9fbbe7536dfeef33dfda16fbb3426889f3f008c9
Joaquinmerida <joaquinmerida1997@gmail.com>
2022-02-24 23:31:10 -0300

    segunda entrega

---
+++ apolo/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div id="header">
+                <h1>Apollo XI</h1>
+                <div className="header__links">
+                    <a href="./../../public/index.html">Inicio</a>
+                    <a href="./../../public/index.html">Ordene aqui</a>
+                    <a href="./../../public/index.html">Contacto</a>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
